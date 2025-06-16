a3ab17c47782befab9062a4d4058736b0db0125b
joseph-abdallah04 <139111723+joseph-abdallah04@users.noreply.github.com>
2025-06-16 16:48:27 +1000

    Finalised configuration
    Added fastAPI and all configuration requirements

--- back_end/main.py
+++ back_end/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("api/test")
+@app.get("/api/test")
 def test_endpoint():
     return {"message": "Hello from back-end!"}
